a4c79587f75f8267c2b1ea0ae2012b4d028b3f4a
David Regla <dreglad@gmail.com>
2018-03-12 07:20:50 -0600

    updates espanol stream

--- bin/ffmpeg_espanol.sh
+++ bin/ffmpeg_espanol.sh
@@ -2,9 +2,9 @@
 
 ffmpeg \
     -hwaccel cuvid \
-    -i udp://225.1.1.8:3000?timeout=600000000 \
-    -g 12 \
-    -c:v h264_nvenc -profile:v main  \
+    -i udp://225.1.1.8:3000 \
+    -g 15 -strict_gop 1 \ -preset:v llhq \
+     -c:v h264_nvenc -profile:v high444p  \
     -c:a libfdk_aac -vbsf h264_mp4toannexb -strict experimental \
     -f flv \
     "rtmp://admin:Webteles2@wsedvr.openmultimedia.biz:1935/live/espanol.stream"
