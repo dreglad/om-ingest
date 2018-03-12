f08b16889a63db988dcf3804f261de1f46be0d35
David Regla <dreglad@gmail.com>
2018-03-12 07:23:27 -0600

    fix

--- bin/ffmpeg_espanol.sh
+++ bin/ffmpeg_espanol.sh
@@ -3,7 +3,7 @@
 ffmpeg \
     -hwaccel cuvid \
     -i udp://225.1.1.8:3000 \
-    -g 15 -strict_gop 1 \ -preset:v llhq \
+    -g 15 -strict_gop 1 -preset:v llhq \
      -c:v h264_nvenc -profile:v high444p  \
     -c:a libfdk_aac -vbsf h264_mp4toannexb -strict experimental \
     -f flv \
