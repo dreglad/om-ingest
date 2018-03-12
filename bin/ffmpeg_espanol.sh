c5025c3e132ee6feb35c9bc0ad9f784b54770779
David Regla <dreglad@gmail.com>
2018-03-12 07:26:38 -0600

    encoding profile

--- bin/ffmpeg_espanol.sh
+++ bin/ffmpeg_espanol.sh
@@ -4,7 +4,7 @@ ffmpeg \
     -hwaccel cuvid \
     -i udp://225.1.1.8:3000 \
     -g 15 -strict_gop 1 -preset:v llhq \
-     -c:v h264_nvenc -profile:v high444p  \
+     -c:v h264_nvenc -profile:v high \
     -c:a libfdk_aac -vbsf h264_mp4toannexb -strict experimental \
     -f flv \
     "rtmp://admin:Webteles2@wsedvr.openmultimedia.biz:1935/live/espanol.stream"
