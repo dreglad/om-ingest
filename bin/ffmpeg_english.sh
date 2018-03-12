204bf362a008de72f455797d2a7ea18fd0983274
David Regla <dreglad@gmail.com>
2018-03-12 07:49:41 -0600

    streams

--- bin/ffmpeg_english.sh
+++ bin/ffmpeg_english.sh
@@ -2,8 +2,7 @@
 
 ffmpeg \
     -hwaccel cuvid \
-    -i udp://225.1.1.2:3000 \
-    -map i:0x411 -map i:0x412 \
+    -i udp://225.1.1.2:3000 -map i:0x411 -map i:0x412 \
     -g 15 -strict_gop 1 -preset:v llhq \
      -c:v h264_nvenc -profile:v high  \
     -c:a libfdk_aac -vbsf h264_mp4toannexb -strict experimental \
